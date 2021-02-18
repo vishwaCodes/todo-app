7c557f4834b320d4c0ffa7d95a1d8dda621c3a99
candytuft <vishnyc3@gmail.com>
2021-02-18 15:30:37 -0500

    Adds Axios API and Todo Model

--- todo-app/src/containers/TodosContainer.js
+++ todo-app/src/containers/TodosContainer.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import TodoModel from '../models/Todo';
 
 class TodosContainer extends Component {
   render () {
+    TodoModel.all().then((res) => {
+      console.log(res);
+    });
     return (
       <div className="todosContainer">
         <h2>This is the Todos Container</h2>
